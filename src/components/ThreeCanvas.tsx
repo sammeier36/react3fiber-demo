2920f38506ec9b355eacfcc4aea3f4bde3686a3c
SamMeier <smeier842@gmail.com>
2025-05-08 15:34:53 -0400

    updated with drei

--- src/components/ThreeCanvas.tsx
+++ src/components/ThreeCanvas.tsx
@@ -1,15 +1,18 @@
+import { Environment, OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 
 export default function ThreeCanvas() {
     return (
-            <Canvas>
+            <Canvas className="draw">
                 <mesh>
-                    <icosahedronGeometry args={[3,100]} />
-                    <meshStandardMaterial color="white" />
+                    <icosahedronGeometry args={[1,16]} />
+                    <meshStandardMaterial color="white" roughness={0} metalness={1}/>
                 </mesh>
-                <directionalLight position={[3, 3, 0]} color={"red"} />
+                {/* <directionalLight position={[3, 3, 0]} color={"red"} />
                 <directionalLight position={[-3, 3, 0]} color={"blue"} />
-                <directionalLight position={[0, 0, 5]} color={"yellow"} />
+                <directionalLight position={[0, 0, 5]} color={"yellow"} /> */}
+                <OrbitControls makeDefault />
+                <Environment preset="city" background/>
         </Canvas>
     );
 }
